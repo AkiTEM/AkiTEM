53f711dd11d2132f68b4e96832ea5200d77cc208
akitem <aquitemappmobile@gmail.com>
2018-09-11 12:44:30 -0300

    Implementando Login. Ainda não está 100%

---
+++ app/src/main/java/br/uninove/akitem/Activity/PrincipalActivity.java
@@ -0,0 +1,16 @@
+package br.uninove.akitem.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.uninove.akitem.R;
+
+public class PrincipalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+
+}

0ab2c5c82b4da62b7a07af0ee2826060b4b48e50
AkiTEM <aquitemappmobile@gmail.com>
2018-09-20 19:11:57 -0300

    Alteração no icone da página home e inclusão da imagem do carrinho.

--- app/src/main/java/br/uninove/akitem/Activity/MainActivity.java
+++ app/src/main/java/br/uninove/akitem/Activity/MainActivity.java
@@ -33,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         toolbar.setLogo(R.drawable.logo);
 
         verifaUsuarioLogado();

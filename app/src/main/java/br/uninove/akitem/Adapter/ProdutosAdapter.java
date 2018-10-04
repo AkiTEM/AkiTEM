e98e190b876740cb9b67753bceefaefd27d2d075
AkiTEM <aquitemappmobile@gmail.com>
2018-10-04 16:16:21 -0300

    Implementando imagem dinâmica no resultado da pesquisa.

--- app/src/main/java/br/uninove/akitem/Adapter/ProdutosAdapter.java
+++ app/src/main/java/br/uninove/akitem/Adapter/ProdutosAdapter.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import br.uninove.akitem.Entidades.Produtos;
+import br.uninove.akitem.Model.Estabelecimento;
 import br.uninove.akitem.R;
 
 public class ProdutosAdapter extends ArrayAdapter<Produtos> {
@@ -34,14 +36,29 @@ public class ProdutosAdapter extends ArrayAdapter<Produtos> {
 
             view = inflater.inflate(R.layout.lista_produtos, parent, false);
 
-            TextView textViewLocal = (TextView) view.findViewById(R.id.textViewLocal);
+            //TextView textViewLocal = (TextView) view.findViewById(R.id.textViewLocal);
             TextView textViewProduto = (TextView) view.findViewById(R.id.textViewProduto);
             TextView textViewValor = (TextView) view.findViewById(R.id.textViewValor);
+            ImageView imagem = (ImageView) view.findViewById(R.id.imagem_estabelecimento);
 
-            Produtos produtos2 = produto.get(position);
-            textViewLocal.setText(produtos2.getEstabaleciomento());
-            textViewProduto.setText(produtos2.getProduto());
-            textViewValor.setText("R$ " + Double.valueOf(produtos2.getValor()).toString().replace(".", ",").concat("0"));
+            Produtos produtos = produto.get(position);
+            //textViewLocal.setText(produtos.getEstabaleciomento());
+            textViewProduto.setText(produtos.getProduto());
+            textViewValor.setText("R$ " + Double.valueOf(produtos.getValor()).toString().replace(".", ",").concat("0"));
+
+            String estabelecimento = produtos.getEstabaleciomento();
+
+            if (estabelecimento.equals(Estabelecimento.ASSAI)) {
+                imagem.setImageResource(R.drawable.assai);
+            } else if (estabelecimento.equals(Estabelecimento.ATACADÃO)) {
+                imagem.setImageResource(R.drawable.atacadao);
+            } else if (estabelecimento.equals(Estabelecimento.CARREFOUR)) {
+                imagem.setImageResource(R.drawable.carrefour);
+            } else if (estabelecimento.equals(Estabelecimento.EXTRA)) {
+                imagem.setImageResource(R.drawable.extra);
+            } else if (estabelecimento.equals(Estabelecimento.ROLDÃO)) {
+                imagem.setImageResource(R.drawable.roldao);
+            }
         }
 
         return view;
